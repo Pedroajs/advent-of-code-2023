3de39813829cb9fbaabc97cc7e298dd528332904
Pedro Augusto <pedroajs93@gmail.com>
2023-12-03 21:35:36 -0300

    Advent of code solution problem 1.

--- src/main/java/org/example/problem1/Trebuchet.java
+++ src/main/java/org/example/problem1/Trebuchet.java
@@ -1,20 +1,39 @@
 package org.example.problem1;
 
-//The newly-improved calibration document consists of lines of text;
-// each line originally contained a specific calibration value that the Elves now need to recover.
-// On each line, the calibration value can be found by combining the first digit and the last digit (in that order)
-// to form a single two-digit number.
-//
-//        For example:
-//
-//        1abc2
-//        pqr3stu8vwx
-//        a1b2c3d4e5f
-//        treb7uchet
-//        In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
-
-
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Trebuchet {
+    public static void trebuchet(String filePath){
+        Long totalSum = 0L;
+        try{
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for(String line : lines){
+                totalSum += getInts(line);
+            }
+            System.out.println("The calibration number is: " + totalSum);
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+    private static Long getInts(String line){
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(line);
+        Integer firstInt = null;
+        Integer lastInt = null;
+
+        while (matcher.find()){
+            int integerValue = Integer.parseInt(matcher.group());
+            if(firstInt == null) firstInt = integerValue;
+            lastInt = integerValue;
+        }
+        if(firstInt != null){
+            return Long.parseLong(firstInt + lastInt.toString());
+        }
+        return 0L;
+    }
 }
